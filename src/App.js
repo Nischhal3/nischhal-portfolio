dba9694a34c3175e4b0c31933c272ed7221b68d0
Nischhal Shrestha <nischhal3@gmail.com>
2024-02-18 21:04:53 +0200

    added navigations

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
+import React from "react";
+import { Link, Route, Routes } from "react-router-dom";
+
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Project from "./components/Project";
+import Experience from "./components/Experience";
 import "./App.css";
+
+const navBar = [
+  { id: 1, title: "About" },
+  { id: 2, title: "Experience" },
+  { id: 3, title: "Project" },
+  { id: 4, title: "Contact" },
+];
 
 const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>My-Portfolio</p>
-      </header>
-    </div>
+    <>
+      <nav>
+        {navBar.map((navItem) => (
+          <li key={navItem.id} to>
+            <Link to={navItem.id === 1 ? "/" : `/${navItem.title}`}>
+              {navItem.title}
+            </Link>
+          </li>
+        ))}
+        <ul></ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<About />} />
+        <Route path="/experience" element={<Experience />} />
+        <Route path="/project" element={<Project />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </>
   );
 };
 
